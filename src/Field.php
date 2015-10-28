6cc8960f0bbd01cd60b2351dcd02ce02cde4f3d0
Sonia Marquette <smarquette@pixelcircus.ca>
2015-10-28 15:38:09 -0400

    Bugfix Invalid id for keys with []  / closes #5

--- src/Field.php
+++ src/Field.php
@@ -123,7 +123,7 @@ class Field
     public function getId()
     {
         if (!isset($this->attributes['id'])) {
-            $this->attributes['id'] = uniqid($this->name, true);
+            $this->attributes['id'] = uniqid(str_replace(['[', ']'], '_', $this->name), true);
         }
         return  $this->attributes['id'];
     }
